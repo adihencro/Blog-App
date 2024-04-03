039150f3a7bad844e95c8c341dfcac0944ec4160
adihencro <adihencro1412@gmail.com>
2024-04-03 17:45:02 +0300

    register and login successful

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,76 +2,84 @@ import React from 'react';
 import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-import { useState } from 'react'; 
+import { useState, useEffect } from 'react';
+import { BLOG_API_URL } from '../../api';
 
 const LoginForm = () => {
 
-    const [formData, setFormData] = useState({
-        username: '',
-        password: '',
-    });
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [success, setSuccess] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
 
-    const handleChange = (event) => {
-        setFormData({ ...formData, [event.target.name]: event.target.value });
-    };
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setIsLoading(true);
-        setError(null); 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setError(null);
 
-        console.log({formData});
-    
-        /* 
-               try {
-          const response = await fetch('/api/register', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formData),
-          });
-    
-          if (!response.ok) {
+    console.log({ formData });
+          
+    try {
+      const response = await fetch(`${BLOG_API_URL}/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.log(errorData);
+        if (errorData && errorData.non_field_errors) {
+            throw new Error(`${errorData.non_field_errors}`);
+        } else {
             throw new Error(`API request failed with status ${response.status}`);
-          }
-    
-          const data = await response.json();
-          setSuccess('Login successful!'); 
-        } catch (error) {
-          setError(error.message);
-        } finally {
-          setIsLoading(false);
         }
-        */
- 
-      };
+      }
+      else {
+        setSuccess("Login successful!");
+        setTimeout(() => {
+            setSuccess('');
+            }, 1800);
+            console.log(response.json())
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    return (
-        <div className='wrapper'>
-            <form action='' onSubmit={ handleSubmit }>
-                <h1>Login</h1>
-                <div className='input-box'>
-                    <input type='text' placeholder='Username' id="username" name="username" value={formData.username} onChange={handleChange} required />
-                    <FaUser className='icon' />
-                </div>
-                <div className='input-box'>
-                    <input type='password' placeholder='Password' id="password" name="password" value={formData.password} onChange={handleChange} required />
-                    <FaLock className='icon' />
-                </div>
-                <button type='submit' disabled={isLoading}>
-                    {isLoading ? 'Logging...' : 'Login'}
-                </button>
-                <div className='register-link'>
-                    <p>Don't have an account? <Link to='/register'>Register</Link></p>
-                </div>
-                {error && <p className="error">{error}</p>}
-                {success && <p className="success">{success}</p>}
-            </form>
+  return (
+    <div className='wrapper'>
+      <form action='' onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <div className='input-box'>
+          <input type='text' placeholder='Username' id="username" name="username" value={formData.username} onChange={handleChange} required />
+          <FaUser className='icon' />
         </div>
-    );
+        <div className='input-box'>
+          <input type='password' placeholder='Password' id="password" name="password" value={formData.password} onChange={handleChange} required />
+          <FaLock className='icon' />
+        </div>
+        <button type='submit' disabled={isLoading}>
+          {isLoading ? 'Logging...' : 'Login'}
+        </button>
+        <div className='register-link'>
+          <p>Don't have an account? <Link to='/register'>Register</Link></p>
+        </div>
+        {error && <p className="error">{error}</p>}
+        {success && <p className="success">{success}</p>}
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -6,40 +6,103 @@ import { BsPencilFill } from "react-icons/bs";
 import { FaPencilAlt } from "react-icons/fa";
 import { BiOutline } from "react-icons/bi";
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BLOG_API_URL } from '../../api';
 
 const RegisterForm = () => {
+
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        bio: '',
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
+
+    const handleChange = (event) => {
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setIsLoading(true);
+        setError(null);
+
+        console.log({ formData });
+
+        try {
+            const response = await fetch(`${BLOG_API_URL}/users/register`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                if (errorData && errorData.username) {
+                    throw new Error(`Username: ${errorData.username.join(', ')}`);
+                } else if (errorData && errorData.email) {
+                    throw new Error(`Email: ${errorData.email.join(', ')}`);
+                } else {
+                    throw new Error(`API request failed with status ${response.status}`);
+                }
+            }
+            else {
+                setSuccess("Registered successfully");
+                setTimeout(() => {
+                    setSuccess('');
+                  }, 1800);
+                  console.log(response.json())
+            }
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+
     return (
         <div className='wrapper'>
-            <form action=''>
+            <form action='' onSubmit={handleSubmit}>
                 <h1>Register</h1>
                 <div className='input-box'>
-                    <input type='text' placeholder='Username' required />
-                    <FaUser className='icon'/>
+                    <input type='text' placeholder='Username' id="username" name="username" value={formData.username} onChange={handleChange} required />
+                    <FaUser className='icon' />
                 </div>
                 <div className='input-box'>
-                    <input type='password' placeholder='Password' required />
-                    <FaLock className='icon'/>
+                    <input type='password' placeholder='Password' id="password" name="password" value={formData.password} onChange={handleChange} required />
+                    <FaLock className='icon' />
                 </div>
                 <div className='input-box'>
-                    <input type='text' placeholder='First Name' required />
-                    <BsPencilFill className='icon'/>
+                    <input type='text' placeholder='First Name' id="first_name" name="first_name" value={formData.first_name} onChange={handleChange} required />
+                    <BsPencilFill className='icon' />
                 </div>
                 <div className='input-box'>
-                    <input type='text' placeholder='Last Name' required />
-                    <FaPencilAlt className='icon'/>
+                    <input type='text' placeholder='Last Name' id="last_name" name="last_name" value={formData.last_name} onChange={handleChange} required />
+                    <FaPencilAlt className='icon' />
                 </div>
                 <div className='input-box'>
-                    <input type='email' placeholder='Email' required />
-                    <MdEmail className='icon'/>
+                    <input type='email' placeholder='Email' id="email" name="email" value={formData.email} onChange={handleChange} required />
+                    <MdEmail className='icon' />
                 </div>
                 <div className='input-box'>
-                    <input type='text' placeholder='Bio' required />
-                    <BiOutline className='icon'/>
+                    <input type='text' placeholder='Bio' id="bio" name="bio" value={formData.bio} onChange={handleChange} required />
+                    <BiOutline className='icon' />
                 </div>
-                <button type='subimt'>Register</button>
+                <button type='submit' disabled={isLoading}>
+                    Register
+                </button>
                 <div className='register-link'>
                     <p>Already have an account? <Link to='/'>Login</Link></p>
                 </div>
+                {error && <p className="error">{error}</p>}
+                {success && <p className="success">{success}</p>}
             </form>
         </div>
     );
